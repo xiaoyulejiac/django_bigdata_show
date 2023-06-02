22edfabe9d8d7ab469d4f6385e28c48f2617a3ff
xiaoyulejia <xiaoyulejiac@gmail.com>
2023-06-03 00:05:00 +0800

    Update urls.py

--- dtest/urls.py
+++ dtest/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from data import views
+from django.conf.urls.static import static
 
 
 urlpatterns = [
